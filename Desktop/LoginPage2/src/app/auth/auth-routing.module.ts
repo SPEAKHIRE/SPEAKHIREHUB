a85987f224845705149fcad0a370b9158463b83a
pikaka <senuang.cai9h@gmail.com>
2021-08-14 19:12:04 -0400

    Initial Commit

---
+++ Desktop/LoginPage2/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+
+
+const routes: Routes = [
+  {path: 'login' , component: LoginComponent},
+  {path: 'register' , component: RegisterComponent},
+  {path: 'reset-password' , component: ResetPasswordComponent},
+
+    {
+      path: '',
+      component: RegisterComponent
+    },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes),RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
