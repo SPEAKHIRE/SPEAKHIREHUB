7a50e08b8cf869abf4959086bd6e9d7b7072e738
pikaka <senuang.cai9h@gmail.com>
2021-08-16 15:38:13 -0400

    commit2

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -15,20 +15,22 @@ export class RegisterComponent implements OnInit {
   step: any = 1;
   submitted: any = false;
   
+
+  
   multistep = new FormGroup({
     confirm: new FormGroup({
-      confirmEligible: new FormControl('',Validators.required),
+      confirmEligible: new FormControl('',[Validators.required, Validators.requiredTrue]),
 
   }),
     profileInformation: new FormGroup({
-      fname: new FormControl('',Validators.required),
+      fname: new FormControl('my name',Validators.required),
       lname: new FormControl('',Validators.required),
       uname: new FormControl('',Validators.required),
       dBirth: new FormControl('',Validators.required),
-      passWord: new FormControl('',Validators.required),
-      reenterPassWord: new FormControl('',Validators.required),
-      email: new FormControl('',Validators.required),
-      pNumber: new FormControl('',Validators.required),
+      passWord: new FormControl('',[Validators.required, Validators.minLength(8)]),
+      reenterPassWord: new FormControl('',[Validators.required, Validators.minLength(8)]),
+      email: new FormControl(null,[Validators.required, Validators.email]),
+      pNumber: new FormControl('',[Validators.required, Validators.pattern('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$')]),
       gender: new FormControl('',Validators.required),
       citizenship: new FormControl('',Validators.required),
       religion: new FormControl('',Validators.required),
@@ -65,8 +67,14 @@ export class RegisterComponent implements OnInit {
     
   }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    // this.multistep = new FormGroup({
+    //   confirm: new FormGroup({
+    //     confirmEligible: new FormControl(null,Validators.required),
+    //   })
+    // });
   }
+
 
 //   get userDetails() {
 //     return this.multistep.controls['userDetails']['controls'];
